2584a7a88e39689d4231326cd9f0e327b19943a3
Nguyễn Trọng Tấn <nguyentrongtan124@gmail.com>
2018-04-05 09:36:16 +0700

    script install Queens

---
+++ Queens/com-1-environment.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#Author Nguyen Trong Tan
+
+source function.sh
+source config.sh
+
+# Function update and upgrade for COMPUTE
+update_upgrade () {
+	echocolor "Update and Upgrade COMPUTE"
+	sleep 3
+	apt update -y && apt upgrade -y
+}
+
+# Function install crudini
+install_crudini () {
+	echocolor "Install crudini"
+	sleep 3
+	apt install -y crudini
+}
+
+# Function install and config NTP
+install_ntp () {
+	echocolor "Install NTP"
+	sleep 3
+
+	apt install chrony -y
+	ntpfile=/etc/chrony/chrony.conf
+
+	sed -i 's|'"pool 2.debian.pool.ntp.org offline iburst"'| \
+	'"server $HOST_CTL iburst"'|g' $ntpfile
+
+	service chrony restart
+}
+
+# Function install OpenStack packages (python-openstackclient)
+install_ops_packages () {
+	echocolor "Install OpenStack client"
+	sleep 3
+	apt install software-properties-common -y
+	add-apt-repository cloud-archive:queens -y
+	apt update -y && apt dist-upgrade -y
+
+	apt install python-openstackclient -y
+}
+
+#######################
+###Execute functions###
+#######################
+
+# Update and upgrade for COMPUTE
+update_upgrade
+
+# Install crudini
+install_crudini
+
+# Install and config NTP
+install_ntp
+
+# OpenStack packages (python-openstackclient)
+install_ops_packages
---
+++ Queens/config.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#Author Nguyen Trong Tan
+
+##########################################
+#### Set local variable  for scripts #####
+##########################################
+
+echocolor "Set local variable for scripts"
+sleep 3
+
+# Network model (provider or selfservice)
+network_model=selfservice
+
+#  Ipaddress variable and Hostname variable
+## Assigning IP for controller node
+CTL_EXT_IP=172.16.68.50
+CTL_EXT_NETMASK=255.255.255.0
+CTL_EXT_IF=ens3
+CTL_MGNT_IP=192.168.30.50
+CTL_MGNT_NETMASK=255.255.255.0
+CTL_MGNT_IF=ens4
+
+## Assigning IP for Compute1 host
+COM1_EXT_IP=172.16.68.51
+COM1_EXT_NETMASK=255.255.255.0
+COM1_EXT_IF=ens3
+COM1_MGNT_IP=192.168.30.51
+COM1_MGNT_NETMASK=255.255.255.0
+COM1_MGNT_IF=ens4
+
+## Assigning IP for Compute2 host
+COM2_EXT_IP=172.16.68.52
+COM2_EXT_NETMASK=255.255.255.0
+COM2_EXT_IF=ens3
+COM2_MGNT_IP=192.168.30.52
+COM2_MGNT_NETMASK=255.255.255.0
+COM2_MGNT_IF=ens4
+
+## Gateway for EXT network
+GATEWAY_EXT_IP=172.16.68.51
+CIDR_EXT=172.16.68.0/24
+CIDR_MGNT=192.168.30.0/24
+
+## Hostname variable
+HOST_CTL=controller
+HOST_COM1=compute1
+HOST_COM2=compute2
+
+# Password variable
+DEFAULT_PASS="tan124"
+
+ADMIN_PASS=$DEFAULT_PASS
+DEMO_PASS=$DEFAULT_PASS
+RABBIT_PASS=$DEFAULT_PASS
+KEYSTONE_DBPASS=$DEFAULT_PASS	
+GLANCE_DBPASS=$DEFAULT_PASS	
+GLANCE_PASS=$DEFAULT_PASS	
+METADATA_SECRET=$DEFAULT_PASS	
+NEUTRON_DBPASS=$DEFAULT_PASS	
+NEUTRON_PASS=$DEFAULT_PASS	
+NOVA_PASS=$DEFAULT_PASS	
+NOVA_DBPASS=$DEFAULT_PASS	
+PLACEMENT_PASS=$DEFAULT_PASS	
